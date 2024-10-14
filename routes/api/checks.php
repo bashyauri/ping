58f504f92069eaee5d8e3e4a5d76577d0940f6e4
Bashar Umar <basharu83@gmail.com>
2024-10-14 02:42:41 +0100

    add routes

--- routes/api/checks.php
+++ routes/api/checks.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Checks\DeleteController;
+use App\Http\Controllers\Checks\IndexController;
+use App\Http\Controllers\Checks\ShowController;
+use App\Http\Controllers\Checks\StoreController;
+use App\Http\Controllers\Checks\UpdateController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',)->name('index');
-Route::post('/',)->name('store');
-Route::get('{checks}',)->name('show');
-Route::put('{check}',)->name('update');
-Route::delete('{check}',)->name('delete');
+Route::get('/', IndexController::class)->name('index');
+Route::post('/', StoreController::class)->name('store');
+Route::get('{checks}', ShowController::class)->name('show');
+Route::put('{checks}', UpdateController::class)->name('update');
+Route::delete('{checks}', DeleteController::class)->name('delete');
--- routes/api/credentials.php
+++ routes/api/credentials.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Credentials\DeleteController;
+use App\Http\Controllers\Credentials\IndexController;
+use App\Http\Controllers\Credentials\ShowController;
+use App\Http\Controllers\Credentials\StoreController;
+use App\Http\Controllers\Credentials\UpdateController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',)->name('index');
-Route::post('/',)->name('store');
-Route::get('{credentials}',)->name('show');
-Route::put('{credential}',)->name('update');
-Route::delete('{credential}',)->name('delete');
+Route::get('/', IndexController::class)->name('index');
+Route::post('/', StoreController::class)->name('store');
+Route::get('{credential}', ShowController::class)->name('show');
+Route::put('{credential}', UpdateController::class)->name('update');
+Route::delete('{credential}', DeleteController::class)->name('delete');
