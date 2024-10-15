5ec30a1a9aa61c8a98bb5139dfa247a6754ed5af
Bashar Umar <basharu83@gmail.com>
2024-10-15 09:52:07 +0100

    completed writing test todos

---
+++ tests/Feature/Checks/ShowTest.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+todo('an unauthenticated user gets the correct status code.');
+todo('a user will recieve the correct status code if the resource doest not exist.');
+todo('an authenticated user cannot view a resource they do not own.');
+todo('an authenticated user gets the correct payload.');
---
+++ tests/Feature/Services/UpdateTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+todo('an unauthenticated user gets the correct status code.');
+todo('an authenticated user cannot update a resource they do not won.');
+todo('the request will be validated.');
+todo('an authenticated user can update their own resources');
+todo('the response comes back in expected format.');
