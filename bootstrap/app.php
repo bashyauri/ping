bf471a0c70b85c8b2524bb01cc032324d9de5f15
Bashar Umar <basharu83@gmail.com>
2024-10-26 13:27:13 +0100

    install error package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,10 +2,18 @@
 
 declare(strict_types=1);
 
-use App\Http\Middleware\SunsetMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Foundation\Application;
+use Treblle\ApiResponses\Data\ApiError;
+use Treblle\ErrorCodes\Enums\ErrorCode;
+use App\Http\Middleware\SunsetMiddleware;
+use JustSteveKing\Tools\Http\Enums\Status;
+use Treblle\ApiResponses\Responses\ErrorResponse;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,5 +36,15 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->render(fn(NotFoundHttpException|ModelNotFoundException $exception, Request $request) => new ErrorResponse(
+            data: new ApiError(
+                title: 'Not Found',
+                detail: $exception->getMessage(),
+                instance: $request->path(),
+                code: ErrorCode::NOT_FOUND->value,
+                link: 'https://docs.domain.com/errors/not-found',
+            ),
+            status: Status::NOT_FOUND,
+        ));
     })->create();
