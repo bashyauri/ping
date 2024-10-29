d96bddb395759817ceb9408769c90161ef6d4ba0
Bashar Umar <basharu83@gmail.com>
2024-10-29 14:33:17 +0100

    completed internationalization

--- app/Http/Controllers/V1/Services/ShowController.php
+++ app/Http/Controllers/V1/Services/ShowController.php
@@ -4,9 +4,28 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\V1\Services;
 
+use App\Http\Resources\V1\ServiceResource;
+use App\Models\Service;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpFoundation\Response;
 
 final class ShowController
 {
-    public function __invoke() {}
+    public function __invoke(Request $request, Service $service): Response
+    {
+        if (!Gate::allows('view', $service)) {
+            throw new UnauthorizedException(
+                message: __('services.v1.show.failure'),
+                code: Response::HTTP_FORBIDDEN,
+            );
+        }
+        return new JsonResponse(
+            data: new ServiceResource(
+                resource: $service
+            )
+        );
+    }
 }
--- lang/en/services.php
+++ lang/en/services.php
@@ -7,10 +7,14 @@ return [
             'success' => 'Your service will be created in the background.',
             'failure' => 'You must verify your email before creating a new service',
         ],
+        'show' => [
+            'failure' => 'You are not able to view a service that yo do not own.',
+        ],
         'update' => [
             'success' => 'We will update your service in the background.',
             'failure' => 'You are not able to update a service that you do not own.',
         ],
+
         'delete' => [
             'success' => 'Your service will be deleted in the background.',
             'failure' => 'You cannot delete a service that you do not own.',
